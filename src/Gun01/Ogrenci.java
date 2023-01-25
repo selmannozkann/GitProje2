720baba259d82742aaaa0a53c59bb8bfdd8444a2
SELMAN <selman45ozkan@gmail.com>
2023-01-25 21:49:54 +0300

    ikinci görevimi yaptım

--- src/Gun01/Ogrenci.java
+++ src/Gun01/Ogrenci.java
@@ -3,6 +3,11 @@ package Gun01;
 public class Ogrenci {
     public static void main(String[] args) {
 
+        for (int i = 0; i < 5; i++) {
+            System.out.println("merhaba");
+
+        }
+
 
 
     }
